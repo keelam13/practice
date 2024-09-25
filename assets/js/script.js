470e33be3572ada02a7afe466d1a9d6d0c377d66
Keevin Aroco <gavikde2022@gmail.com>
2024-09-25 22:26:04 +0000

    Add display function code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,47 @@
-function runQuiz() {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button")
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("class") === "start-btn") {
+                displayQuestion();
+            } else {
+                checkAnswer();
+            }
+        })
+    }
+})
+
+function displayQuestion() {
+    
+    let body = document.getElementsByTagName('body')[0];
+
+    let img = document.getElementsByTagName("img")[0];
+    img.src = "assets/images/rx.png";
+
+    let choicesBtn = document.getElementsByClassName("choices-btn");
+    choicesBtn.style.display = "visible";
+
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        body.style.flexDirection = "row";
+        img.style.width = "50%";
+        img.style.height = "50%";
+    } else {
+        body.style.flexDirection = "column";
+        img.style.width = "30%";
+        img.style.height = "30%";
+    }
+
+    let questions = ["What?", "Where?", "When?", "Who?", "Why?"];
+    let question = Math.floor(Math.random() * questions.length);
+    let paragraph = document.getElementsByTagName("p")[0];
+    paragraph.textContent = `${questions[question]}`;
+
+    let answers = ["Dolor", "Santiago City", "July 3, 2024", "Birthday", "Party"];
+    let choices = Math.floor(Math.random() * answers.length);
+    
+
+
 
 }
 
@@ -14,14 +57,10 @@ function incrementScore() {
 
 }
 
-function displayQuestion() {
-
-}
-
 function displayBonusQuestion() {
     
 }
 
 function multipleChoice () {
-    
+
 }
